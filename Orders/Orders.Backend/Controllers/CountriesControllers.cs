23ff3ab8c6ea94853863ed9a1375c27dd8a78fad
mateomarquez915 <mateomarquez315389@correo.itm.edu.co>
2025-08-23 18:07:41 -0500

    Add PUT and GET for countries

---
+++ Orders/Orders.Backend/Controllers/CountriesControllers.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Orders.Backend.Data;
+using Orders.Shared.Entities;
+
+namespace Orders.Backend.Controllers;
+
+[ApiController]
+[Route("api/[Controller]")]
+public class CountriesControllers : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public CountriesControllers(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet] //para obtener todos los datos//
+    public async Task<IActionResult> GetAsync()
+    {
+        return Ok(await _context.Countries.ToListAsync()); //devuelve la lista de paises//
+    }
+
+    [HttpPost] //para crear un nuevo dato//
+    public async Task<IActionResult> PostAsync(Country country)
+    {
+        _context.Countries.Add(country);//para agregar un nuevo pais//
+        await _context.SaveChangesAsync();//para guardar los cambios en la base de datos//
+        return Ok(country); //devuelve el pais creado//
+    }
+}
