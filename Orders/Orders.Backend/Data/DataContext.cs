8b417bd893ad0f9cc44f9db52f4b89107f0deaca
mateomarquez915 <mateomarquez315389@correo.itm.edu.co>
2025-08-23 17:36:50 -0500

    Add countries entity

---
+++ Orders/Orders.Backend/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Orders.Shared.Entities;
+
+namespace Orders.Backend.Data;
+
+public class DataContext : DbContext //me va a representar la bade de datos completa//
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options) //Constructor que recibe las opciones de configuracion//
+    {
+    }
+
+    public DbSet<Country> Countries { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) //Metodo que se ejecuta cuando se crea el modelo de datos//
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();//Agrega datos iniciales a la tabla Country//
+    }
+}
---
+++ Orders/Orders.Shared/Entities/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Orders.Shared.Entities;
+
+public class Country
+{
+    public int Id { get; set; }
+
+    [Display(Name = "Pais")] // Representa al name como el nombre pais//
+    [MaxLength(100, ErrorMessage = "EL campo {0} no puede tener mas de {1} caracteres.")] //capacidad que va a tener en caracteres y muestra el error que esta//
+    [Required(ErrorMessage = "El campo {0] es obligatorio.")]
+    public string Name { get; set; } = null!; //No permite nulos en el campo//
+}
